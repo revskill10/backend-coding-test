0c64d9bb327c3589b12ad921ea4853fbc39c3446
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-10-04 17:08:43 +0700

    [4/typeorm] Fix blogModule

--- serverless-blog/src/apps/blog/blog.module.ts
+++ serverless-blog/src/apps/blog/blog.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as models from 'src/domain/models';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([models.User])],
+  imports: [TypeOrmModule.forFeature([models.User, models.Article])],
   exports: [TypeOrmModule]
 })
 export class BlogModule {}
