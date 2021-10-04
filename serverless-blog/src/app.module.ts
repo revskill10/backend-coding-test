ce2f1a50398b0aa229eb673fd4a873df5ee5a2e2
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-10-04 14:37:45 +0700

    [2/firebase] Add firebase auth

--- serverless-blog/src/app.module.ts
+++ serverless-blog/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseAuthStrategy } from './firebase/firebase-auth.strategy';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirebaseAuthStrategy],
 })
 export class AppModule {}
