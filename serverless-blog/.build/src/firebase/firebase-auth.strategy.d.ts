4c3c5cd9aedaccee1c9c52812ecc9e4febc19c28
Truong Hoang Dung <truonghoangdung1987hp@gmail.com>
2021-10-04 15:10:22 +0700

    [3/serverless] ignore .build

--- serverless-blog/.build/src/firebase/firebase-auth.strategy.d.ts
+++
@@ -1,7 +0,0 @@
-declare const FirebaseAuthStrategy_base: new (...args: any[]) => any;
-export declare class FirebaseAuthStrategy extends FirebaseAuthStrategy_base {
-    private defaultApp;
-    constructor();
-    validate(token: string): Promise<any>;
-}
-export {};
